2b02f8d98a39aea24c249198406ffdbcfaad234e
Michael Günter <michaelguenter@Michaels-MacBook-Pro.local>
2019-07-25 14:50:18 +0200

    Project 04: fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,70 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// Pseudo code:
+// while (true) {
+//     if (keyboard == 0) fill = 0 (white) else fill = -1 (black)
+//     addr = SCREEN
+//     for (i=0; i<8192; i++) {
+//         RAM[addr] = fill;
+//         addr++;
+//     }
+// }
+
+(START)
+    @KBD
+    D=M
+    @FILLWHITE
+    D;JEQ // if keyboard == 0 then fill white else fill black
+    
+(FILLBLACK)
+    @fill
+    M=-1 // fill = -1
+
+    @FILL
+    0;JMP // goto FILL
+
+(FILLWHITE)
+    @fill
+    M=0 // fill = 0
+
+    @FILL
+    0;JMP // goto FILL
+
+(FILL)
+    @SCREEN
+    D=A
+    @addr
+    M=D // addr = SCREEN
+
+    @0
+    D=A
+    @i
+    M=D // i = 0
+
+(LOOP)
+    @8192 // n = 8192 (512x256 screen resolution -> 16Bit * 32 * 256 -> 32*256=8192)
+    D=A
+    @i
+    D=D-M
+    @END
+    D;JEQ // if i == n goto END
+
+    @fill
+    D=M
+    @addr
+    A=M
+    M=D // M[addr] = fill
+
+    @addr
+    M=M+1 // addr++
+
+    @i
+    M=M+1 // i++
+
+    @LOOP
+    0;JMP // goto LOOP
+
+(END)
+    @START
+    0;JMP // infinite loop
