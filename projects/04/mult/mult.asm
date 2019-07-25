7c2786bc429bf8822e04a5091dd320c0557a4f22
Michael Günter <michaelguenter@Michaels-MacBook-Pro.local>
2019-07-25 13:58:25 +0200

    Project 04: mult

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,38 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Pseudo code:
+// R2 = 0;
+// for (i=0; i<R0; i++) {
+//     R2 += R1;
+// }
+
+    @0
+    D=A
+    @R2
+    M=D // R2 = 0
+    @i
+    M=D // i = 0
+
+(LOOP)
+    @R0
+    D=M
+    @i
+    D=D-M
+    @END
+    D;JEQ // if i == R0 goto END
+
+    @R1
+    D=M
+    @R2
+    M=M+D // R2 += R1
+
+    @i
+    M=M+1 // i++
+
+    @LOOP
+    0;JMP // goto LOOP
+
+(END)
+    @END
+    0;JMP // goto END
